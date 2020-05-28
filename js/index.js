2bd003cc8b9871e61973006c194189799e0cfb9c
slashinfty <matt.braddock@gmail.com>
2020-05-28 19:28:12 -0400

    first stream

---
+++ js/index.js
@@ -0,0 +1,18 @@
+// dead zones: 053-063, 117-127, 181-191, 245-255, 309-319, 373-383, 437-447, 501-511, 545-575, 609-639, 673-703, 736-767, 801-831, 865-895, 929-959, 992-999
+const deadZoneColors = [ //this is 704
+  [255, 232, 207], [223, 144, 79], [175, 40, 32], [48, 24, 80]
+];
+// rewrite this ^
+
+// note: 544=512, 608=576, 672=640, 800=768, 864=832, 928=896
+// 960-991 = 704-735
+
+// https://github.com/dtao/nearest-color
+// http://jscolor.com/download/
+// https://stackoverflow.com/questions/42966641/how-to-transform-black-into-any-given-color-using-only-css-filters/43960991#43960991
+
+// create color lists for "nearest color"
+// page elements
+// create preview pane
+// break down sgb code
+// create sgb code
