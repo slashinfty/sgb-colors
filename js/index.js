59c97f0fb981ca14f45380a2b8f1941a06134858
slashinfty <matt.braddock@gmail.com>
2020-05-29 11:47:17 -0400

    final version

--- js/index.js
+++ js/index.js
@@ -2,19 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
   // create arrays of unique colors for each layer
   // the codes are hex numbers, but we need strings of #dddddd
   colorLookup.forEach(code => {
-    if (code.hasOwnProperty('color')) {
-      // some codes are a single color
-      let c = '#' + ("00000" + code.color.toString(16)).substr(-6); // leading zeros trick
-      if (!color1.includes(c)) color1.push(c);
-      if (!color2.includes(c)) color2.push(c);
-      if (!color3.includes(c)) color3.push(c);
-      if (!color4.includes(c)) color4.push(c);
-    } else {
-      // some codes vary by layer
-      if (!color1.includes('#' + ("00000" + code.colors[0].toString(16)).substr(-6))) color1.push('#' + ("00000" + code.colors[0].toString(16)).substr(-6));
-      if (!color2.includes('#' + ("00000" + code.colors[1].toString(16)).substr(-6))) color2.push('#' + ("00000" + code.colors[1].toString(16)).substr(-6));
-      if (!color3.includes('#' + ("00000" + code.colors[2].toString(16)).substr(-6))) color3.push('#' + ("00000" + code.colors[2].toString(16)).substr(-6));
-      if (!color4.includes('#' + ("00000" + code.colors[3].toString(16)).substr(-6))) color4.push('#' + ("00000" + code.colors[3].toString(16)).substr(-6));
+    for (let i = 0; i < 4; i++) {
+      if (code.hasOwnProperty('color')) { // some codes are a single color
+        if (!window["color" + (i + 1).toString()].includes('#' + ("00000" + code.color.toString(16)).substr(-6))) window["color" + (i + 1).toString()].push('#' + ("00000" + code.color.toString(16)).substr(-6));
+      } else { // some codes vary by layer
+        if (!window["color" + (i + 1).toString()].includes('#' + ("00000" + code.colors[i].toString(16)).substr(-6))) window["color" + (i + 1).toString()].push('#' + ("00000" + code.colors[i].toString(16)).substr(-6));
+      }
     }
   });
   
@@ -24,10 +17,20 @@ document.addEventListener("DOMContentLoaded", () => {
   for (let i = 0; i < imageEls.length; i++) imageEls[i].style.filter = filterColor(colorEls[i].jscolor.rgb);
 
   // set up nearest color functions
-  nearestColor1 = nearestColor.from(color1);
-  nearestColor2 = nearestColor.from(color2);
-  nearestColor3 = nearestColor.from(color3);
-  nearestColor4 = nearestColor.from(color4);
+  for (let i = 1; i <= 4; i++) window["nearestColor" + i.toString()] = nearestColor.from(window["color" + i.toString()]);
+
+  // get code from url
+  // ?sgb=XXXX-XXXX-XXXX
+  let url = new URL(window.location.href);
+  if (url.searchParams.has('sgb')) {
+    let sgbCode = url.searchParams.get('sgb');
+    document.getElementById("sgb-code").value = sgbCode;
+    if (!/^\d{4}-\d{4}-\d{4}$/.test(sgbCode)) { //check if code is in valid format
+      alert(sgbCode + " is not a valid code");
+      return;
+    } else codeToColor(sgbCode);
+  }
+
 });
 
 function deadZone(code) {
@@ -48,228 +51,21 @@ function duplicateCodes(code) {
   else return code;
 }
 
-class Color { //code from https://stackoverflow.com/questions/42966641/how-to-transform-black-into-any-given-color-using-only-css-filters/43960991#43960991
-  constructor(r, g, b) { this.set(r, g, b); }
-  toString() { return `rgb(${Math.round(this.r)}, ${Math.round(this.g)}, ${Math.round(this.b)})`; }
-
-  set(r, g, b) {
-    this.r = this.clamp(r);
-    this.g = this.clamp(g);
-    this.b = this.clamp(b);
-  }
-
-  hueRotate(angle = 0) {
-    angle = angle / 180 * Math.PI;
-    let sin = Math.sin(angle);
-    let cos = Math.cos(angle);
-
-    this.multiply([
-      0.213 + cos * 0.787 - sin * 0.213, 0.715 - cos * 0.715 - sin * 0.715, 0.072 - cos * 0.072 + sin * 0.928,
-      0.213 - cos * 0.213 + sin * 0.143, 0.715 + cos * 0.285 + sin * 0.140, 0.072 - cos * 0.072 - sin * 0.283,
-      0.213 - cos * 0.213 - sin * 0.787, 0.715 - cos * 0.715 + sin * 0.715, 0.072 + cos * 0.928 + sin * 0.072
-    ]);
-  }
-
-  grayscale(value = 1) {
-    this.multiply([
-      0.2126 + 0.7874 * (1 - value), 0.7152 - 0.7152 * (1 - value), 0.0722 - 0.0722 * (1 - value),
-      0.2126 - 0.2126 * (1 - value), 0.7152 + 0.2848 * (1 - value), 0.0722 - 0.0722 * (1 - value),
-      0.2126 - 0.2126 * (1 - value), 0.7152 - 0.7152 * (1 - value), 0.0722 + 0.9278 * (1 - value)
-    ]);
-  }
-
-  sepia(value = 1) {
-    this.multiply([
-      0.393 + 0.607 * (1 - value), 0.769 - 0.769 * (1 - value), 0.189 - 0.189 * (1 - value),
-      0.349 - 0.349 * (1 - value), 0.686 + 0.314 * (1 - value), 0.168 - 0.168 * (1 - value),
-      0.272 - 0.272 * (1 - value), 0.534 - 0.534 * (1 - value), 0.131 + 0.869 * (1 - value)
-    ]);
-  }
-
-  saturate(value = 1) {
-    this.multiply([
-      0.213 + 0.787 * value, 0.715 - 0.715 * value, 0.072 - 0.072 * value,
-      0.213 - 0.213 * value, 0.715 + 0.285 * value, 0.072 - 0.072 * value,
-      0.213 - 0.213 * value, 0.715 - 0.715 * value, 0.072 + 0.928 * value
-    ]);
-  }
-
-  multiply(matrix) {
-    let newR = this.clamp(this.r * matrix[0] + this.g * matrix[1] + this.b * matrix[2]);
-    let newG = this.clamp(this.r * matrix[3] + this.g * matrix[4] + this.b * matrix[5]);
-    let newB = this.clamp(this.r * matrix[6] + this.g * matrix[7] + this.b * matrix[8]);
-    this.r = newR; this.g = newG; this.b = newB;
-  }
-
-  brightness(value = 1) { this.linear(value); }
-  contrast(value = 1) { this.linear(value, -(0.5 * value) + 0.5); }
-
-  linear(slope = 1, intercept = 0) {
-    this.r = this.clamp(this.r * slope + intercept * 255);
-    this.g = this.clamp(this.g * slope + intercept * 255);
-    this.b = this.clamp(this.b * slope + intercept * 255);
-  }
-
-  invert(value = 1) {
-    this.r = this.clamp((value + (this.r / 255) * (1 - 2 * value)) * 255);
-    this.g = this.clamp((value + (this.g / 255) * (1 - 2 * value)) * 255);
-    this.b = this.clamp((value + (this.b / 255) * (1 - 2 * value)) * 255);
-  }
-
-  hsl() { // Code taken from https://stackoverflow.com/a/9493060/2688027, licensed under CC BY-SA.
-    let r = this.r / 255;
-    let g = this.g / 255;
-    let b = this.b / 255;
-    let max = Math.max(r, g, b);
-    let min = Math.min(r, g, b);
-    let h, s, l = (max + min) / 2;
-
-    if(max === min) {
-      h = s = 0;
-    } else {
-      let d = max - min;
-      s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-      switch(max) {
-        case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-        case g: h = (b - r) / d + 2; break;
-        case b: h = (r - g) / d + 4; break;
-      } h /= 6;
-    }
-
-    return {
-      h: h * 100,
-      s: s * 100,
-      l: l * 100
-    };
-  }
-
-  clamp(value) {
-    if(value > 255) { value = 255; }
-    else if(value < 0) { value = 0; }
-    return value;
-  }
-}
-
-class Solver { //code from https://stackoverflow.com/questions/42966641/how-to-transform-black-into-any-given-color-using-only-css-filters/43960991#43960991
-  constructor(target) {
-    this.target = target;
-    this.targetHSL = target.hsl();
-    this.reusedColor = new Color(0, 0, 0); // Object pool
-  }
-
-  solve() {
-    let result = this.solveNarrow(this.solveWide());
-    return {
-      values: result.values,
-      loss: result.loss,
-      filter: this.css(result.values)
-    };
-  }
-
-  solveWide() {
-    const A = 5;
-    const c = 15;
-    const a = [60, 180, 18000, 600, 1.2, 1.2];
-
-    let best = { loss: Infinity };
-    for (let i = 0; best.loss > 25 && i < 3; i++) {
-      let initial = [50, 20, 3750, 50, 100, 100];
-      let result = this.spsa(A, a, c, initial, 1000);
-      if(result.loss < best.loss) { best = result; }
-    } return best;
-  }
-
-  solveNarrow(wide) {
-    const A = wide.loss;
-    const c = 2;
-    const A1 = A + 1;
-    const a = [0.25 * A1, 0.25 * A1, A1, 0.25 * A1, 0.2 * A1, 0.2 * A1];
-    return this.spsa(A, a, c, wide.values, 500);
-  }
-
-  spsa(A, a, c, values, iters) {
-    const alpha = 1;
-    const gamma = 0.16666666666666666;
-
-    let best = null;
-    let bestLoss = Infinity;
-    let deltas = new Array(6);
-    let highArgs = new Array(6);
-    let lowArgs = new Array(6);
-
-    for (let k = 0; k < iters; k++) {
-      let ck = c / Math.pow(k + 1, gamma);
-      for (let i = 0; i < 6; i++) {
-        deltas[i] = Math.random() > 0.5 ? 1 : -1;
-        highArgs[i] = values[i] + ck * deltas[i];
-        lowArgs[i]  = values[i] - ck * deltas[i];
-      }
-
-      let lossDiff = this.loss(highArgs) - this.loss(lowArgs);
-      for (let i = 0; i < 6; i++) {
-        let g = lossDiff / (2 * ck) * deltas[i];
-        let ak = a[i] / Math.pow(A + k + 1, alpha);
-        values[i] = fix(values[i] - ak * g, i);
-      }
-
-      let loss = this.loss(values);
-      if(loss < bestLoss) { best = values.slice(0); bestLoss = loss; }
-    } return { values: best, loss: bestLoss };
-
-    function fix(value, idx) {
-      let max = 100;
-      if(idx === 2 /* saturate */) { max = 7500; }
-      else if(idx === 4 /* brightness */ || idx === 5 /* contrast */) { max = 200; }
-
-      if(idx === 3 /* hue-rotate */) {
-        if(value > max) { value = value % max; }
-        else if(value < 0) { value = max + value % max; }
-      } else if(value < 0) { value = 0; }
-      else if(value > max) { value = max; }
-      return value;
-    }
-  }
-
-  loss(filters) { // Argument is array of percentages.
-    let color = this.reusedColor;
-    color.set(0, 0, 0);
-
-    color.invert(filters[0] / 100);
-    color.sepia(filters[1] / 100);
-    color.saturate(filters[2] / 100);
-    color.hueRotate(filters[3] * 3.6);
-    color.brightness(filters[4] / 100);
-    color.contrast(filters[5] / 100);
-
-    let colorHSL = color.hsl();
-    return Math.abs(color.r - this.target.r)
-         + Math.abs(color.g - this.target.g)
-         + Math.abs(color.b - this.target.b)
-         + Math.abs(colorHSL.h - this.targetHSL.h)
-         + Math.abs(colorHSL.s - this.targetHSL.s)
-         + Math.abs(colorHSL.l - this.targetHSL.l);
-  }
-
-  css(filters) {
-    function fmt(idx, multiplier = 1) { return Math.round(filters[idx] * multiplier); }
-    return `invert(${fmt(0)}%) sepia(${fmt(1)}%) saturate(${fmt(2)}%) hue-rotate(${fmt(3, 3.6)}deg) brightness(${fmt(4)}%) contrast(${fmt(5)}%)`;
-  }
-}
-
 function filterColor(rgbArray) {
+  // creates CSS filters to approximate layer colors
   let color = new Color(rgbArray[0], rgbArray[1], rgbArray[2]);
   let solver = new Solver(color);
   let result;
-  do {
-    result = solver.solve();
-  } while (result.loss > 5);
+  // find approximate color within a certain tolerance
+  do result = solver.solve();
+  while (result.loss > 5);
   return result.filter;
 }
 
-function colorToCode() {
+function colorToCodeBtn() {
+  let colorEls = document.getElementsByClassName("jscolor");
   let sgbCode = '';
   let nearColors = [];
-  let colorEls = document.getElementsByClassName("jscolor");
   for (let i = 0; i < colorEls.length; i++) {
     // get color from input
     let inputColor = colorEls[i].jscolor.toHEXString().toLowerCase();
@@ -285,13 +81,48 @@ function colorToCode() {
   // format code and set
   let postCode = sgbCode.substr(0, 4) + "-" + sgbCode.substr(4, 4) + "-" + sgbCode.substr(8, 4);
   document.getElementById("sgb-code").value = postCode;
-  //color preview image
+  // color preview image
   let imageEls = document.getElementsByClassName("preview");
-  for (let i = 0; i < imageEls.length; i++) {
-    let hexToRGB = hex => {
-      var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-      return [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)];
-    }
-    imageEls[i].style.filter = filterColor(hexToRGB(nearColors[i]));
+  let hexToRGB = hex => {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)];
   }
+  for (let i = 0; i < imageEls.length; i++) imageEls[i].style.filter = filterColor(hexToRGB(nearColors[i]));
+  shareLink();
+}
+
+function codeToColorBtn() {
+  let sgbCode = document.getElementById("sgb-code").value;
+  if (!/^\d{4}-\d{4}-\d{4}$/.test(sgbCode)) { //check if code is in valid format
+    alert(sgbCode + " is not a valid code");
+    return;
+  } else codeToColor(sgbCode);
+}
+
+function codeToColor(sgb) {
+  // break code into components
+  let sgbComponents = [parseInt(sgb.substr(0, 3)), parseInt(sgb.substr(3, 1) + sgb.substr(5, 2)), parseInt(sgb.substr(7, 2) + sgb.substr(10, 1)), parseInt(sgb.substr(11, 3))];
+  let colorEls = document.getElementsByClassName("jscolor");
+  let imageEls = document.getElementsByClassName("preview");
+  let hexToRGB = hex => {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)];
+  }
+  sgbComponents.forEach((component, i) => {
+    // find the code and get the color
+    let codeObj = colorLookup.find(o => o.value === duplicateCodes(deadZone(component)));
+    // set the colors
+    let clr = codeObj.hasOwnProperty("color") ? '#' + ("00000" + codeObj.color.toString(16)).substr(-6) : '#' + ("00000" + codeObj.colors[i].toString(16)).substr(-6);
+    colorEls[i].jscolor.fromString(clr);
+    // color preview image
+    imageEls[i].style.filter = filterColor(hexToRGB(clr));
+  });
+  shareLink();
+}
+
+function shareLink() {
+  // show link to share
+  let p = document.getElementById("share-link");
+  p.innerText = "Share your color palette: https://mattbraddock/sgb-colors/?sgb=" + document.getElementById("sgb-code").value;
+  p.style.display = "block";
 }
